4b6e953507d79adb65e88b85ae6e7499e3fe4b1d
Antonio <Antonio@DESKTOP-RCLDU18>
2026-03-08 10:37:09 +0100

    Clase Pregunta completa

--- src/modelo/Pregunta.java
+++ src/modelo/Pregunta.java
@@ -1,5 +1,57 @@
 package modelo;
 
-public class Pregunta {
-    
+import java.util.List;
+import java.io.Serializable;
+
+public class Pregunta implements Serializable {
+
+    public enum Tipo {
+        MULTIPLE, TEXTO
+    }
+
+    private String enunciado;
+    private List<String> opciones;
+    private int respuestaCorrecta;
+    private String respuestaTexto;
+    private String imagen;
+    private Tipo tipo;
+
+    public Pregunta(String enunciado, List<String> opciones, int respuestaCorrecta, String imagen) {
+        this.enunciado = enunciado;
+        this.opciones = opciones;
+        this.respuestaCorrecta = respuestaCorrecta;
+        this.imagen = imagen;
+        this.tipo = Tipo.MULTIPLE;
+    }
+
+    public Pregunta(String enunciado, String respuestaTexto, String imagen) {
+        this.enunciado = enunciado;
+        this.respuestaTexto = respuestaTexto;
+        this.imagen = imagen;
+        this.tipo = Tipo.TEXTO;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public List<String> getOpciones() {
+        return opciones;
+    }
+
+    public int getRespuestaCorrecta() {
+        return respuestaCorrecta;
+    }
+
+    public String getRespuestaTexto() {
+        return respuestaTexto;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
 }
