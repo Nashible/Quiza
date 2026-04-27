f7ac156c4f29c776714d072e0a7613a9a18085c2
Antonio <Antonio@DESKTOP-RCLDU18>
2026-04-27 19:28:48 +0200

    La contraseña de la BD ya no se muestra en la clase, para más seguridad ahora esta en un archivo .properties.

--- src/servicios/ConexionBD.java
+++ src/servicios/ConexionBD.java
@@ -1,24 +1,46 @@
 package servicios;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
 /**
- * Clase ConexionBD
- * Se encarga de establecer la conexión con la Base de Datos en linea
+ * Clase ConexionBD Se encarga de establecer la conexión con la Base de Datos en
+ * linea
+ *
  * @author Antonio Donoso
  * @version 1.0
  */
 public class ConexionBD {
 
-    private static final String URL = "jdbc:postgresql://ep-purple-sun-abs2j3jh-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require";
-    private static final String USUARIO = "neondb_owner";
-    private static final String CONTRASENA = "npg_fObuhH7YTCe2";
+    private static String URL;
+    private static String USUARIO;
+    private static String CONTRASENA;
+    //static para usarlo una vez y lo primero al usar la clase
+    static {
+        try {//Creo el objeto properties
+            Properties props = new Properties();
+            //Para leer el contenido desde dentro del proyecto/Jar
+            InputStream is = ConexionBD.class.getResourceAsStream("/recursos/config.properties");
+           //Leemos el contenido del archivo y lo metemos en memoria
+            props.load(is);
+        //Obtenemos cada propiedad del archivo y lo guardamos en variables
+            URL = props.getProperty("db.url");
+            USUARIO = props.getProperty("db.user");
+            CONTRASENA = props.getProperty("db.password");
 
-    
+        } catch (Exception e) {
+            System.err.println("Error cargando configuración de BD");
+            e.printStackTrace();
+        }
+    }
+/**Establece la conexión con Neon Console*/
     public static Connection getConnection() throws SQLException {
         try {
-           //Carga el driver
+            //Carga el driver
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
             System.err.println("Error: Driver PostgreSQL no encontrado.");
